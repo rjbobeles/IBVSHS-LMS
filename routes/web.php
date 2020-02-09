7a9401de732a4bcdf147e685df732aa2330059a9
Ryan Jandrick B. Obeles <51547772+rjbobeles@users.noreply.github.com>
2020-02-09 19:20:49 +0800

    Added Routes

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,39 @@
 |
 */
 
+//Patron Routes
 Route::get('/', function () {
     return view('welcome');
 });
-Auth::routes();
 
+//User Route
+Auth::routes(['verify' => true]);
 Route::get('/home', 'HomeController@index')->name('home');
+
+//Libarian Route
+
+//Admin Routes
+Route::prefix('admin')->middleware('auth', 'verified')->group(function () {
+
+    //System Logs
+    Route::prefix('logs')->group(function () {
+        
+        //User Logs
+        Route::prefix('user')->group(function () {
+            Route::get('/', 'LogUserController@index');
+            Route::get('/{id}', 'LogUserController@show');
+        });
+
+        //Book Logs
+        Route::prefix('book')->group(function () {
+            Route::get('/', 'LogBookController@index');
+            Route::get('/{id}', 'LogBookController@show');
+        });
+
+        //Patron Logs
+        Route::prefix('patron')->group(function () {
+            Route::get('/', 'LogPatronController@index');
+            Route::get('/{id}', 'LogPatronController@show');
+        });
+    });
+});
