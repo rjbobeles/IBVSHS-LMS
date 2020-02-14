e1c7f5d564ee287fc0f98357564cd56c75d9fe94
Ryan Jandrick B. Obeles <51547772+rjbobeles@users.noreply.github.com>
2020-02-14 08:10:56 +0800

    Implemented deactivated account feature

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 class LoginController extends Controller
 {
     /*
@@ -38,6 +41,11 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
+    protected function credentials(Request $request)
+    {        
+       return ['username' => $request->{$this->username()}, 'password' => $request->password, 'deactivated' => "0"];
+    }
+    
     public function username()
     {   
         return 'username';
