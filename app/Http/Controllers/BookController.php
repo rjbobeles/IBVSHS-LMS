2fa02b2e20bc7bb02d1f7b7bb13766ba828bf847
Ryan Jandrick B. Obeles <51547772+rjbobeles@users.noreply.github.com>
2020-02-21 21:38:27 +0800

    Update methods

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -37,61 +37,48 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
-        //Form Validations
-        $this-> validate($request, [
-            'callnumber' => 'required',
-            'title' => 'required',
-            'author' => 'required',
-            'isbn' => 'required',
-            'year_published' => 'required',
-            'publisher' => 'required',
-            'genre' => 'required',
-            'book_image' => 'image|nullable|max:1999'
+        $validate = $request->validate([
+            'callnumber' => ['required'],
+            'title' => ['required'],
+            'author' => ['required'],
+            'isbn' => ['required'],
+            'year_published' => ['required'],
+            'publisher' => ['required'],
+            'genre' => ['required'],
+            'condition' => ['required', 'in:Find,Very Good,Good,Fair,Poor'],
+            'status' => ['required', 'in:Available,Reserved,Borrowed,Archived'],
+            'book_image' => ['image', 'nullable', 'max:200']
         ]);
         
-        //Add Book
-        $book = new Book();
-        $book->callnumber = $request->input('callnumber');
-        $book->title = $request->input('title');
-        $book->author = $request->input('author');
-        $book->isbn = $request->input('isbn');
-        $book->volume = $request->input('volume');
-        $book->edition = $request->input('edition');
-        $book->year_published = $request->input('year_published');
-        $book->publisher = $request->input('publisher');
-        $book->genre = $request->input('genre');
-        $book->condition = $request->input('condition');
-        $book->status = $request->input('status');
-
         $getLastBarcodeNo = DB::table('books')->select('barcodeno')->orderBy('barcodeno', 'desc')->take(1)->first();
-
-        if ($getLastBarcodeNo != null) {
-            $newBarcodeNo = $getLastBarcodeNo->barcodeno + 1;
-            $book->barcodeno = $newBarcodeNo; 
-        } else {
-            $book->barcodeno = 202000000001;
-        }
-
-        //Handle FileUpload
+        if ($getLastBarcodeNo != null) $newBarcodeNo = $getLastBarcodeNo->barcodeno + 1;
+        else $newBarcodeNo = 202000000001;
+        
         if ($request->hasFile('book_image')){
-            //Get filename with extension
             $fileNameWithExt = $request->file('book_image')->getClientOriginalName();
-            //Get just filename
             $filename = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
-            //Get just ext
-            $extension = $request->file('book_image')->getClientOriginalExtension();
-            //Filename to store
-            $fileNameToStore = $filename.'_'.time().'.'.$extension;
-            //Upload Image
+            $Ext = $request->file('book_image')->getClientOriginalExtension();
+            $fileNameToStore = $filename . '_' . time() . '.' . $Ext;
             $path = $request->file('book_image')->storeAs('public/book_images', $fileNameToStore);
-        } else {
-            $fileNameToStore = 'no-image.jpg';
-        }
+        } else $fileNameToStore = 'no-image.jpg';
+        
+        $book = Book::create([
+            'callnumber' => $request->input('callnumber'),
+            'title' => $request->input('title'),
+            'author' => $request->input('author'),
+            'isbn' => $request->input('isbn'),
+            'volume' => $request->input('volume'),
+            'edition' => $request->input('edition'),
+            'year_published' => $request->input('year_published'),
+            'publisher' => $request->input('publisher'),
+            'genre' => $request->input('genre'),
+            'condition' => $request->input('condition'),
+            'status' => $request->input('status'),
+            'barcodeno' => $newBarcodeNo,
+            'book_image' => $fileNameToStore
+        ]);
 
-        $book->book_image = $fileNameToStore;
-        $book->save();
-
-        return redirect('/books');
+        return redirect()->route('books.index')->with('success', 'New book has been added to the system!');
     }
 
     /**
@@ -126,18 +113,19 @@ class BookController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $this->validate($request, [
-            'callnumber' => 'required',
-            'title' => 'required',
-            'author' => 'required',
-            'isbn' => 'required',
-            'year_published' => 'required',
-            'publisher' => 'required',
-            'genre' => 'required',
-            'book_image' => 'image|nullable|max:1999'
+        $validate = $request->validate([
+            'callnumber' => ['required'],
+            'title' => ['required'],
+            'author' => ['required'],
+            'isbn' => ['required'],
+            'year_published' => ['required'],
+            'publisher' => ['required'],
+            'genre' => ['required'],
+            'condition' => ['required', 'in:Find,Very Good,Good,Fair,Poor'],
+            'status' => ['required', 'in:Available,Reserved,Borrowed,Archived'],
+            'book_image' => ['image', 'nullable', 'max:200']
         ]);
-        
-        //Update Book
+         
         $book = Book::find($id);
         $book->callnumber = $request->input('callnumber');
         $book->title = $request->input('title');
@@ -151,29 +139,21 @@ class BookController extends Controller
         $book->condition = $request->input('condition');
         $book->status = $request->input('status');
 
-        //Handle FileUpload
         if ($request->hasFile('book_image')){
-            //Get filename with extension
             $fileNameWithExt = $request->file('book_image')->getClientOriginalName();
-            //Get just filename
             $filename = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
-            //Get just ext
-            $extension = $request->file('book_image')->getClientOriginalExtension();
-            //Filename to store
-            $fileNameToStore = $filename.'_'.time().'.'.$extension;
-            //Upload Image
+            $Ext = $request->file('book_image')->getClientOriginalExtension();
+            $fileNameToStore = $filename . '_' . time() . '.' . $Ext;
             $path = $request->file('book_image')->storeAs('public/book_images', $fileNameToStore);
-        } else {
-            $fileNameToStore = 'no-image.jpg';
-        }
+        } else $fileNameToStore = 'no-image.jpg';
 
         if ($request->hasFile('book_image')){
             Storage::delete('public/book_images/'.$book->book_image);
             $book->book_image = $fileNameToStore;
         }
+    
         $book->save();
-
-        return redirect('/books');
+        return redirect()->route('books.index')->with('success', 'Book has been successfully updated!');
     }
 
     /**
