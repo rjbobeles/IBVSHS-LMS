7358bc0321511f0e48f98ca984ffc04970abd985
Ryan Jandrick B. Obeles <51547772+rjbobeles@users.noreply.github.com>
2020-02-22 15:48:18 +0800

    Updated LogBook fillables

--- app/LogBook.php
+++ app/LogBook.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class LogBook extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'actor_id', 'action', 'book_id', 'callnumber', 'title', 'author', 'isbn', 'volume', 'edition', 'year_published', 'publisher', 'genre', 'condition', 'status', 'barcodeno', 'book_image'
+    ];
+
     ///LogBook <- User
     public function userLogBook() {
         return $this->belongsTo('App\User', 'actor_id', 'id');
