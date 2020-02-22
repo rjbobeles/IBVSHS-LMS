7e9a46b5e7923208cb89c524c5fbcbc79cd2aa1d
Ryan Jandrick B. Obeles <51547772+rjbobeles@users.noreply.github.com>
2020-02-22 15:47:52 +0800

    Added LogUser model

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use App\LogUser;
 use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
