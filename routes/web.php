6f1c038c4e356c4f21b071540ca337a76004726b
Snuckles123 <bernardguiller.reccion@benilde.edu.ph>
2020-02-28 20:14:36 +0800

    updated routes for librarian

--- routes/web.php
+++ routes/web.php
@@ -65,13 +65,21 @@ Route::middleware('auth', 'verified', 'isLibrarian', 'isActive')->group(function
     //Librarian Route
     Route::prefix('librarian')->group(function () {
 
+        //Librarian Homepage
+        Route::get('/', 'HomeController@Librarian')->name('librarian');
+
         //Patrons
         Route::resource('patrons', 'PatronController');
 
         //Book Controller
         Route::resource('books', 'BookController');
 
-        Route::get('/', 'BookController@Index')->name('librarian.books.index');
+        Route::prefix('books')->group(function(){
+            Route::get('/', 'BookController@Index')->name('librarian.books.index');
+            Route::get('/create', 'BookController@create')->name('librarian.books.create');
+            Route::get('/{id}/edit', 'BookController@edit')->name('librarian.books.edit');
+            Route::put('/{id}', 'BookController@update')->name('librarian.books.update');
+        });
 
     });
 });
