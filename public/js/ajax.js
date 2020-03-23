16e328856b454f672606a974d56805d29ba88f5d
Ryan Jandrick B. Obeles <51547772+rjbobeles@users.noreply.github.com>
2020-03-23 15:00:44 +0800

    Removed edit, added fade out on blur

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -121,10 +121,6 @@ $(document).ready(function () {
       });
     }
   });
-});
-$(document).on('click', '#patronList li', function () {
-  $('#borrower').val($(this).text());
-  $('#patronList').fadeOut();
 }); //Autocomplete ajax for searching book to be borrowed in librarian.transactions.create
 
 $(document).ready(function () {
@@ -155,74 +151,18 @@ $(document).ready(function () {
     }
   });
 });
-$(document).on('click', '#bookList li', function () {
-  $('#book').val($(this).text());
-  $('#bookList').fadeOut();
-}); //Autocomplete ajax for searching borrower name in librarian.transactions.edit
-
-$(document).ready(function () {
-  $('#borrower').keyup(function () {
-    var query = $(this).val();
-
-    if (query != '') {
-      var _token = $('input[name="_token"]').val();
-
-      $.ajaxSetup({
-        headers: {
-          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-      });
-      $.ajax({
-        url: "edit/fetchPatron",
-        method: "POST",
-        data: {
-          query: query,
-          _token: _token
-        },
-        dataType: "json",
-        success: function success(data) {
-          $('#patronList').fadeIn();
-          $('#patronList').html(data);
-        }
-      });
-    }
-  });
-});
 $(document).on('click', '#patronList li', function () {
   $('#borrower').val($(this).text());
   $('#patronList').fadeOut();
-}); //Autocomplete ajax for searching book to be borrowed in librarian.transactions.create
-
-$(document).ready(function () {
-  $('#book').keyup(function () {
-    var query = $(this).val();
-
-    if (query != '') {
-      var _token = $('input[name="_token"]').val();
-
-      $.ajaxSetup({
-        headers: {
-          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-      });
-      $.ajax({
-        url: "edit/fetchBook",
-        method: "POST",
-        data: {
-          query: query,
-          _token: _token
-        },
-        dataType: "json",
-        success: function success(data) {
-          $('#bookList').fadeIn();
-          $('#bookList').html(data);
-        }
-      });
-    }
-  });
 });
 $(document).on('click', '#bookList li', function () {
   $('#book').val($(this).text());
+  $('#bookList').fadeOut();
+});
+document.getElementById("borrower").addEventListener("blur", function () {
+  $('#patronList').fadeOut();
+});
+document.getElementById("book").addEventListener("blur", function () {
   $('#bookList').fadeOut();
 });
 
