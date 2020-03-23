97ae874e3343c91726f33aa62e3b1abedf89bfe0
Ryan Jandrick B. Obeles <51547772+rjbobeles@users.noreply.github.com>
2020-03-23 22:02:18 +0800

    Updated patron routes

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,9 @@
 */
 
 Route::middleware('guest')->group(function() {
-    Route::get('/', function () { return view('patron');})->name('patron');
+    Route::get('/', 'HomeController@patron')->name('patron');
+    Route::get('/books', 'BookController@PatronBooks')->name('patron.book.index');
+    Route::get('/books/{id}', 'BookController@PatronBook')->name('patron.book.single');
 
 });
 
