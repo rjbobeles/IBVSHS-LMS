444d4ba13617dd5cf846f96767d19d28c810d62c
Ryan Jandrick B. Obeles <51547772+rjbobeles@users.noreply.github.com>
2020-03-23 15:01:56 +0800

    Added log transaction fillable

--- app/LogTransaction.php
+++ app/LogTransaction.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class LogTransaction extends Model
 {
+    protected $fillable = [
+        'actor_id', 'action', 'transaction_id', 'patron_id', 'book_id', 'date_issued', 'date_due', 'date_returned'
+    ];
+
     //LogTransaction <- User
     public function userLogTransaction() {
         return $this->belongsTo('App\User', 'actor_id', 'id');
